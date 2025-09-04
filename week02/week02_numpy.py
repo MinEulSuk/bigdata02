3101791f8fa9ac848b37da96c025abc782438df5
202130208 <202130208@daelim.ac.kr>
2025-09-04 16:07:23 +0900

    v0.6 numpy

--- week02/week02_numpy.py
+++ week02/week02_numpy.py
@@ -5,21 +5,11 @@
 
 import numpy as np
 
-#arr = np.array(["Korean","English","Mathmatics"])
-#arr = np.array([[1,2,3],[4,5,6]])
-arr = np.array(
-    [
-        [
-            [1.0,2.0,3.0],
-            [4.2,5.9,9.1],
-        ],
-        [
-            [4.0, 6.8, 8.2],
-            [8.2, 4.1, 6.5],
-        ]
-    ]
-)
-print(arr)
-print(arr.shape,arr.dtype,arr.ndim,arr.size)
-#위 3차원 배열에 4.1만 출력
-print(arr[1,1,1])#arr[1][1][1] << 이것도 가능은 한데 파이썬은 보통 콤마로 함
+arr = np.array([1,2,3,4,5])
+# indexing
+print(arr[2])
+print(arr[4],arr[-1],arr[len(arr)-1])
+
+#slicing
+print(arr[1:3])
+print(arr[::3])

62f7dff7f57ef39bd06e1a493eab9b745d28dac6
202130208 <202130208@daelim.ac.kr>
2025-09-11 15:29:09 +0900

    v0.5 pandas

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -3,18 +3,13 @@ import pandas as pd
 df4 = pd.DataFrame({
     'date':['2025-09-11','2025-09-11','2025-09-12','2025-09-13 '],
     'city':['서울', '안양', '서울', '안양'],
-    'tmp':[23, 22, 24, 22]},
+    'tmp':[23, 22, 24, 26]},
     index=[1, 2, 3, 4]
 )
 print(df4)
-# df7 = df4.drop(columns=['date','city']) #drop specific columns
-# print(df7)
-# df8 = df4[df4.tmp <= 23]
-# print(df8)
-#df8 = df4.duplicated() # 중복값 찾기
-#데이터 분석을 할때 결측값이 반드시 있기때문에 생각을 해야함
-#pivot함수를 사용했을때 결측값이 생긴다.
-#결측치 처리에대한 대처가 필수불가결하다.
+df9 =df4.sample(n=2) # 중복되지않은 row 2개 랜덤하게 추출
+print(df9)
+df10 = df4.nsmallest(2,'tmp')  # 가장 작은 값 두개 nlagrest면 큰값 두개
+print(df10)
+#tail 원본기준 아래서 n개  head 원본기준 위에서 n개 기본5개
 
-df6 = df4.pivot(index='date',)
-print(df8)

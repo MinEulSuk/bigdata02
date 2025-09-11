ac54d6e4c0927423478932957124b34c285e3e06
202130208 <202130208@daelim.ac.kr>
2025-09-11 14:52:35 +0900

    v0.7 numpy

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -38,3 +38,15 @@ df3 = pd.melt(df2).rename(columns={
     }).query('val > 5')
 
 print(df3)
+
+import pandas as pd
+
+df4 = pd.DataFrame({
+    '날짜':['2025-09-11','2025-09-11','2025-09-12','2025-09-12'],
+    '도시':['서울', '안양', '서울', '안양'],
+    '온도':[23, 22, 24, 26]},
+    index=[1, 2, 3, 4]
+)
+print(df4)
+df5 = df4.pivot(index='날짜', columns='도시', values='온도')
+print(df5)

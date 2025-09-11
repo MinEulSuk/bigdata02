48b0252b67952edaf6ae0193b8e4fbcd0c943295
202130208 <202130208@daelim.ac.kr>
2025-09-11 15:23:34 +0900

    v0.4 pandas

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -1,11 +1,20 @@
 import pandas as pd
 
 df4 = pd.DataFrame({
-    '날짜':['2025-09-11','2025-09-11','2025-09-12','2025-09-12'],
-    '도시':['서울', '안양', '서울', '안양'],
-    '온도':[23, 22, 24, 26]},
+    'date':['2025-09-11','2025-09-11','2025-09-12','2025-09-13 '],
+    'city':['서울', '안양', '서울', '안양'],
+    'tmp':[23, 22, 24, 22]},
     index=[1, 2, 3, 4]
 )
 print(df4)
-df6 = df4.sort_values('온도',ascending=False) #desc
-print(df6)
+# df7 = df4.drop(columns=['date','city']) #drop specific columns
+# print(df7)
+# df8 = df4[df4.tmp <= 23]
+# print(df8)
+#df8 = df4.duplicated() # 중복값 찾기
+#데이터 분석을 할때 결측값이 반드시 있기때문에 생각을 해야함
+#pivot함수를 사용했을때 결측값이 생긴다.
+#결측치 처리에대한 대처가 필수불가결하다.
+
+df6 = df4.pivot(index='date',)
+print(df8)

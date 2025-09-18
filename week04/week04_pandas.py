2fb0b20bcf60ce49b39d925f3caec37f4d274f6d
202130208 <202130208@daelim.ac.kr>
2025-09-18 15:49:36 +0900

    v0.8 pandas

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -3,26 +3,15 @@ import pandas as pd
 import seaborn as sns
 
 mpg = sns.load_dataset("mpg")
-# print(mpg.info())
+print(mpg.value_counts())#
 
-# print(mpg.head(3))
-# print(mpg.tail(3))
-# mpg_desc = mpg.sort_values(by=['mpg'],ascending = False)
-# print(mpg_desc)
-#print(mpg.describe())
-
-print(mpg[mpg['mpg'] > 40]) # 조건식으로도 가능 이게 쿼리문보다 빠름
-print(mpg.query('mpg > 40')) # 쿼리문으로도 가능 이게 조건문보다 느림 함수호출에 걸리는 시간때문에
-# print(mpg.drop_duplicates()) # 칼럼 기준으로 중복값 없애기
-
-#print(mpg.sample(n = 5)) # 랜덤으로 5개 행 출력
-#print(mpg.nlargest(5, 'mpg')) # mpg 칼럼 기준으로 큰값 5개 출력
-#print(mpg.nsmallest(5, 'mpg')) # mpg 칼럼 기준으로 작은값 5개 출력
-
-#print(mpg[['displacement', 'horsepower','weight','acceleration','model_year']]) # 너무 길다
-#slice로 처리하면 추출할 수 있다.
-#print(mpg.iloc[:,2:7]) # 2~6까지 출력 # iloc는 위치기반 인덱싱 위에 것과 동일하게 작동
-#print(mpg.loc[:, 'displacement':'model_year']) # loc는 라벨기반 인덱싱 - 가독성이 좋음
-
-#print(mpg.filter(regex='.me')) # me로 끝나는 칼럼명 추출
+df = pd.DataFrame({
+    'city': ['anyang', 'seoul', 'seoul', 'anyang','seoul', 'incheon'],
+    'name': ['tom','jerry','park','kim','park','lee'],
+    },index = [1,2,3,4,5,6]
+)
+print(df)
+# print(df.value_counts()) # index를 제외한 모든 컬럼의 값이 동일한 행의 개수를 세어줌 즉 중복값
+# print(df.shape) # (6, 2)
+print(df['city'].nunique()) # 도시가 몇개인지 세기 # 3
 

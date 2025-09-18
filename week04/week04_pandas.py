18f062e0e2f10c91f5ad476528f44b9936031ad5
202130208 <202130208@daelim.ac.kr>
2025-09-18 16:45:48 +0900

    v1.2 pandas nan ver3

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -13,6 +13,10 @@ mpg = sns.load_dataset('mpg')
 # 결측치 처리 #2
 # mpg = mpg.fillna(mpg['horsepower'].mean())
 # print(mpg[mpg['horsepower'].isnull()]) #결측값이 하나라도 있는 행 출력
+
+# 결측치 처리 #3 결측치가 있는 컬럼을 날려버림
+#칼럼을 날리려면 빅데이터로서 필요가 없는 데이터(이름) or 결측치가 너무 많아서 어쩔 수 없이 날려야 하는 경우
+mpg.drop(columns =['horsepower'], axis=1, inplace=True) #원본에 반영
 print(mpg.info())
 
 

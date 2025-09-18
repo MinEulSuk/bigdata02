d47897ab87149979ec8af0f120d963edb581f341
202130208 <202130208@daelim.ac.kr>
2025-09-18 16:34:56 +0900

    v1.1 pandas nan

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -4,7 +4,15 @@ import seaborn as sns
 #결측값을 어떻게 추출할 거냐?
 mpg = sns.load_dataset('mpg')
 # print(mpg.isnull().sum()) #컬럼별 결측값 개수
-# print(mpg.isnull()) #각 데이터별 결측값 여부
 # print(mpg[mpg['horsepower'].isnull()]) #결측값이 하나라도 있는 행 추출
-mpg_nan = mpg[mpg['horsepower'].isnull()]
-print(mpg_nan[['mpg','horsepower','displacement','origin']]) #결측값이 있는 행의 특정 컬럼들만 추출
+
+# 결측치 처리 #1
+# mpg_drop = mpg.dropna()
+# print(mpg_drop[mpg_drop['horsepower'].isnull()]) #결측값이 하나라도 있는 행 출력
+
+# 결측치 처리 #2
+# mpg = mpg.fillna(mpg['horsepower'].mean())
+# print(mpg[mpg['horsepower'].isnull()]) #결측값이 하나라도 있는 행 출력
+print(mpg.info())
+
+

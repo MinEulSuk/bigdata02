16d03f0bbff0493141874ba0bf56f2261a49b646
202130208 <202130208@daelim.ac.kr>
2025-09-18 19:16:32 +0900

    week07 v1.7 pandas

--- week07/week07_pandas.py
+++ week07/week07_pandas.py
@@ -2,17 +2,32 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-print(sns.get_dataset_names())
-
-
 titanic = sns.load_dataset('titanic')
-# print(titanic.head())
-# print(titanic.describe())
+plt.figure(figsize=(15,12))
+plt.subplot(2,3,1)# 2 rows, 3 columns, 1st plot
+sns.boxplot(data=titanic,x="survived",y="age")# 박스플롯으로 표현
+plt.title('Age distribution by survival status')# 생존에 따른 나이 분포
+plt.xlabel("Survived (0 = No, 1 = Yes)")
+plt.ylabel("Age")
 
-#print(titanic[titanic['age'] <= 15])
+plt.subplot(2,3,2)# 2 rows, 3 columns, 2nd plot
+sns.boxplot(data=titanic,x="sex",y="age")# 박스플롯으로 표현
+plt.title('Age distribution by gender')# 성별에 따른 나이 분포
+plt.xlabel("gender")
+plt.ylabel("Age")
 
-#nlagrest를 통해 나이가 많은 5명 출력
-print(titanic.nlargest(5, 'age'))
+plt.subplot(2,3,3)# 2 rows, 3 columns, 2nd plot
+sns.boxplot(data=titanic,x="class",y="fare")# 박스플롯으로 표현
+plt.title('fare distribution by class') # 클래스에 따른 요금 분포
+plt.xlabel("Passenger class")
+plt.ylabel("Fare")
+
+plt.subplot(2,3,4)# 2 rows, 3 columns, 2nd plot
+sns.boxplot(data=titanic,x="survived",y="fare")# 박스플롯으로 표현
+plt.title('Fare distribution by survival status')# 요금에 따른 생존 분포
+plt.xlabel("Survived (0 = No, 1 = Yes)")
+plt.ylabel("Fare")
+plt.show()
 
 
 

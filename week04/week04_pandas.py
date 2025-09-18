98db0b89d46ed0461964b1d4ee4bf8901f7e9fc4
202130208 <202130208@daelim.ac.kr>
2025-09-18 16:23:25 +0900

    v1.0 pandas

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -1,23 +1,10 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-
-def square(x):
-    return x * x
-
-def cube(x):
-    return x * x * x
-
+#결측값을 어떻게 추출할 거냐?
 mpg = sns.load_dataset('mpg')
-df1 = pd.DataFrame(
-    {
-        'A': [1, 2, 3],
-        'B': [4, 5, 6],
-        'C': [7, 8, 9],
-
-    }
-    , index=['1', '2', '3']
-)
-print(df1)
-print(df1.apply(lambda x : x*x)) # 각 원소에 제곱 함수 적용
-print(df1.apply(lambda x : x**3)) # 각 원소에 세제곱 함수 적용
+# print(mpg.isnull().sum()) #컬럼별 결측값 개수
+# print(mpg.isnull()) #각 데이터별 결측값 여부
+# print(mpg[mpg['horsepower'].isnull()]) #결측값이 하나라도 있는 행 추출
+mpg_nan = mpg[mpg['horsepower'].isnull()]
+print(mpg_nan[['mpg','horsepower','displacement','origin']]) #결측값이 있는 행의 특정 컬럼들만 추출

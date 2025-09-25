7c1bb4286284b49425f909a1541eb77511f8eca0
202130208 <202130208@daelim.ac.kr>
2025-09-25 16:38:41 +0900

    v0.2 ml (custom module)

--- week05/week05_ml.py
+++ week05/week05_ml.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import tglearn as tg
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
@@ -9,14 +10,13 @@ df = pd.read_csv('life_satisfaction.csv')
 X = df[["GDP per capita (USD)"]].values
 y = df[["Life satisfaction"]].values
 
-model1 = LinearRegression()
-model2 = KNeighborsRegressor(3)
-
+# model1 = LinearRegression()
+# model2 = KNeighborsRegressor(3)
+model1 = tg.LinearRegression()
+model2 = tg.KNeighborsRegressor()
 model1.fit(X, y)
 model2.fit(X, y)
 new_instance = [[31_721.3]] # South Korea GDP per capita in 2020
 print(f'Life Satisfaction(Linear Regression) : {model1.predict(new_instance)}')
 print(f'Life Satisfaction(Kneighors Regression) : {model2.predict(new_instance)}')
-print(model1.predict(new_instance))
-print(model2.predict(new_instance))
 
